7990abdec841b201804c1368df82a53b714589d1
Sudar-Kudr <melihovivan936@gmail.com>
2021-05-22 13:25:07 +0300

    Initial Commit

---
+++ git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git
+//
+//  Created by User on 22.05.2021.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
